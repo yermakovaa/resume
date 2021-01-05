86d8365b0b2482f47ea0107b3ee7f7c804c2d524
Anastasiia Yermakova <63540687+yermakovaa@users.noreply.github.com>
2021-01-05 23:46:51 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Container from './components/Container';
+
+function App() {
+  return <Container></Container>;
+}
+
+export default App;
---
+++ src/components/Container/Container.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import s from './Container.module.css';
+
+function Container({ children }) {
+  return <div className={s.container}>{children}</div>;
+}
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Container;
